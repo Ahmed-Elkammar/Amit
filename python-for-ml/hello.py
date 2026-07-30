56f4c053be2e211dcbfc2f887559e2c62b068eff
Ahmed-Elkammar <ahmadfwz1412@gmail.com>
2026-07-30 15:37:56 +0300

    edited the code

---
+++ python-for-ml/hello.py
@@ -0,0 +1,6 @@
+print ("Welcome to my first Python program!")
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+print("The sum is:", num1 + num2)
